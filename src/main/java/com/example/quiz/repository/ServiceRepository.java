a4aadf2f5e6be163cf9e6da5a8e2526148bbbaf1
decagon <godsonjonathan39@gmail.com>
2022-01-10 10:22:38 +0100

    first Commit

--- src/main/java/com/example/quiz/repository/ServiceRepository.java
+++ src/main/java/com/example/quiz/repository/ServiceRepository.java
@@ -1,4 +1,20 @@
 package com.example.quiz.repository;
 
+import com.example.quiz.model.Solution;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class ServiceRepository {
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    public ServiceRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Solution saveProduct(Solution solution){
+        String sql = "INSERT INTO solution(name, questionTitle) VALUES (?, ?)";
+        Object[] params = new Object[] {solution.getName(), solution.questionTitle()};
+        jdbcTemplate.update(sql,params);
+        return solution;
+    }
 }
